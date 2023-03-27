c89708ce6f326371dd906597cfd2926de24ad13f
Graziano Statello <grazianos86@gmail.com>
2023-03-27 12:06:38 +0100

    step 2 remove default route

--- src/start-here/index.js
+++ src/start-here/index.js
@@ -4,10 +4,6 @@ import { users } from './routes/users.js'
 export function createServer() {
   const app = fastify()
 
-  app.get('/', async () => {
-    return { hello: 'world' }
-  })
-
   app.register(users)
 
   return app
