deec812d23252550ffe22b082e9fe05984aa98d8
Graziano Statello <grazianos86@gmail.com>
2023-03-27 16:45:24 +0100

    step 6

---
+++ src/start-here/test/users.test.js
@@ -0,0 +1,12 @@
+import t from 'tap'
+import { createServer } from '../index.js'
+
+t.test('GET /users', async t => {
+  const app = await createServer()
+  const response = await app.inject({
+    method: 'GET',
+    url: '/users',
+  })
+  t.equal(response.statusCode, 200)
+  t.same(response.json(), { users: ['john', 'jane'] })
+})
